137e2240a18d22b5f1c18f5465e34f1ae5d89038
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-07 16:21:10 +0300

    В файле манифест переопределили метку, чтобы на панели приложения выводился нужный текст (из указанного в строковом файле). Для другой активити.

---
+++ app/src/main/java/com/omelchenkoaleks/pizza/OrderActivity.java
@@ -0,0 +1,17 @@
+package com.omelchenkoaleks.pizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
