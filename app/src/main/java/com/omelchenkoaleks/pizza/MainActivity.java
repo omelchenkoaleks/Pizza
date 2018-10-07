ff5d8ebe83cf9acbd3b8045c6c4c307e2d181af1
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-07 16:08:20 +0300

    Приказыаем активити использовать панель действий как панель приложения.

--- app/src/main/java/com/omelchenkoaleks/pizza/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/pizza/MainActivity.java
@@ -2,6 +2,7 @@ package com.omelchenkoaleks.pizza;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // получаем ссылку на панель инструментов
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        // назначаем панель инструментов панелью действий активности
+        setSupportActionBar(toolbar);
     }
 }
