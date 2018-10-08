1d3e599813bdda26a13ce1b648d0bdc88ebc4a08
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-08 15:42:53 +0300

    добавили кнопку вверх

--- app/src/main/java/com/omelchenkoaleks/pizza/OrderActivity.java
+++ app/src/main/java/com/omelchenkoaleks/pizza/OrderActivity.java
@@ -1,5 +1,6 @@
 package com.omelchenkoaleks.pizza;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -13,5 +14,10 @@ public class OrderActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // необходимо использовать этот метод из библиотеки поддержки
+        ActionBar actionBar = getSupportActionBar();
+        // включаем кнопку вверх
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
