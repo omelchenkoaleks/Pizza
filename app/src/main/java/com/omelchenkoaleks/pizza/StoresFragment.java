e413f0198e27bfb34481ba7ac564463bf2ee816c
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-10 07:03:11 +0300

    добавлен четвертый фрагмент

---
+++ app/src/main/java/com/omelchenkoaleks/pizza/StoresFragment.java
@@ -0,0 +1,32 @@
+package com.omelchenkoaleks.pizza;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class StoresFragment extends ListFragment {
+
+
+    public StoresFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.stores));
+        setListAdapter(arrayAdapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
