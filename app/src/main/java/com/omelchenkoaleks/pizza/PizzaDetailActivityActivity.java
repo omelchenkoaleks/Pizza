dd60d1925a40d23a18dcd49e54b68d9dd697dac6
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-11 11:17:04 +0300

    создана ативити для вывода детализации и макет

---
+++ app/src/main/java/com/omelchenkoaleks/pizza/PizzaDetailActivityActivity.java
@@ -0,0 +1,13 @@
+package com.omelchenkoaleks.pizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PizzaDetailActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_detail_activity);
+    }
+}
