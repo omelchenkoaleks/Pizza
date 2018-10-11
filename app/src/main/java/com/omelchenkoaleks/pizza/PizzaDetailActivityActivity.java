dd91ffb291ccb4130cbce64bb47d689cc8401922
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-11 12:01:44 +0300

    код PizzaDetailActivity выводит названия и изображения пиццы, выбранные пользователем - для этого извлекаем id из интента запустившего активность

--- app/src/main/java/com/omelchenkoaleks/pizza/PizzaDetailActivityActivity.java
+++ app/src/main/java/com/omelchenkoaleks/pizza/PizzaDetailActivityActivity.java
@@ -1,13 +1,41 @@
 package com.omelchenkoaleks.pizza;
 
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PizzaDetailActivityActivity extends AppCompatActivity {
+
+    // будем использовать для передачи id d дополнительной информации интента
+    public static final String EXTRA_PIZZA_ID = "pizzaId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pizza_detail_activity);
+        setContentView(R.layout.activity_pizza_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // добавляем кнопку вверх
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // показываем детали пиццы
+        // идентификатор используется для заполнения TextView and ImageView
+        int pizzaId = (Integer) getIntent().getExtras().get(EXTRA_PIZZA_ID);
+
+        String pizzaName = Pizza.pizzas[pizzaId].getName();
+        TextView textView = findViewById(R.id.pizza_text);
+        textView.setText(pizzaName);
+
+        int pizzaImage = Pizza.pizzas[pizzaId].getImageResourceId();
+        ImageView imageView = findViewById(R.id.pizza_image);
+        imageView.setImageDrawable(ContextCompat.getDrawable(this, pizzaImage));
+        imageView.setContentDescription(pizzaName);
     }
 }
